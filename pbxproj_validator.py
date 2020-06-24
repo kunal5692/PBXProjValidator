6ece9c90ae740d7420ee43d567f4c6c6137a1d4b
kunal-d11 <kunal.chavhan@dream11.com>
2020-06-24 18:19:50 +0530

    Add method description

--- pbxproj_validator.py
+++ pbxproj_validator.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 import sys
 
-stack = []
+# Stack to keep track of parenthesis
+validator_stack = []
+# Stack to keep track of each parenthesis line number
+validator_stack_line = []
+# Lists of valid open parenthesis
 open_list = ["{","("] 
+# List of valid closed parenthesis
 close_list = ["}",")"]
+# Variable to keep track of line count
+line_count = 0
 
+"""
+Main method
+"""
 def main(argv):
     if len(sys.argv) < 2:
         print("Please provide file path")
@@ -14,28 +24,45 @@ def main(argv):
     print(pbxproj_path)
     verify(pbxproj_path)
 
+"""
+Method to verify pxproj file
+Parameters: Path of pxproj file
+"""
 def verify(filepath):
     fh = open(filepath, "r")
     while True:
+        global line_count
+        line_count += 1
         line = fh.readline()
+        if not line:
+            break
         parse(line)
-
-
-def parse(myStr):
-    for i in myStr: 
-        if i in open_list: 
-            stack.append(i) 
-        elif i in close_list: 
-            pos = close_list.index(i) 
-            if ((len(stack) > 0) and
-                (open_list[pos] == stack[len(stack)-1])): 
-                stack.pop() 
-            else: 
-                return "Unbalanced"
-    if len(stack) == 0: 
-        return "Balanced"
+    fh.close()
+    if len(validator_stack) == 0: 
+        print("Validation Completed!!!")
+        sys.exit(0)
     else: 
-        return "Unbalanced"
+        print("Validation completed with error")
+        sys.exit(1)
+
+"""
+Method to balance parenthesis
+Parameter: Pbxproj line
+"""
+def parse(line):
+    for char in line: 
+        if char in open_list: 
+            global line_count
+            validator_stack.append(char) 
+            validator_stack_line.append(line_count)
+        elif char in close_list: 
+            if ((len(validator_stack) > 0) and
+                (char == validator_stack[len(validator_stack)-1])): 
+                validator_stack.pop()
+                validator_stack_line.pop()
+            else:
+                print("Detected error at line: " + str(validator_stack_line[len(validator_stack) - 1]))
+                sys.exit(1)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
